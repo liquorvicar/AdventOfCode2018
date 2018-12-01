2685eb7ad0b4cd09f9075d682a62df012ad16001
Andy Clyde <me@andyclyde.com>
2018-12-01 08:14:28 +0000

    Refactoring

--- src/day1/main.ts
+++ src/day1/main.ts
@@ -4,12 +4,12 @@ import {createLogger} from "bunyan";
 const inputs = readFileSync('./src/day1/input.txt', { encoding: 'ascii' });
 const log = createLogger({ name: 'AoC2018-1', level: 'debug' });
 
-let start = 0;
-const values = inputs.split("\n").filter(value => value !== "");
-for (const value in values) {
-    const change = parseInt(values[value], 10);
-    log.debug(change);
-    start+= change;
+let frequency = 0;
+const changes: number[] = inputs.split("\n")
+    .filter(value => value !== "")
+    .map((change): number => parseInt(change, 10));
+for (const change of changes) {
+    frequency+= change;
 }
 
-log.debug({ start }, 'Answer');
+log.debug({ start: frequency }, 'Answer');
