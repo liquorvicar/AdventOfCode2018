a89e74246e599e4c740b60d3fc2f2a586b2cbd25
Andy Clyde <me@andyclyde.com>
2018-12-06 14:21:30 +0000

    Day 6 part 1

---
+++ src/day6/main.ts
@@ -0,0 +1,104 @@
+import Logger = require('bunyan');
+
+type Coordinates = { id: string, column: number, row: number }[];
+
+export const findLimits = (coordinates: Coordinates): number[] => {
+    const limits = [0, 0];
+    coordinates.forEach(coordinate => {
+        if (coordinate.column > limits[0]) {
+            limits[0] = coordinate.column;
+        }
+        if (coordinate.row > limits[1]) {
+            limits[1] = coordinate.row;
+        }
+    });
+    return limits;
+};
+
+export const detectClosestCoordinate = (coordinates: Coordinates, column: number, row: number): string => {
+    const distances = coordinates.map((coordinate) => {
+        return {
+            distance: Math.abs(coordinate.column - column) + Math.abs(coordinate.row - row),
+            id: coordinate.id
+        };
+    }).sort((a, b) => {
+        return a.distance < b.distance ? -1 : 1;
+    });
+    if (distances[0].distance === distances[1].distance) {
+        return '.';
+    }
+    return distances[0].id;
+};
+
+export const mapGrid = (coordinates: Coordinates): string[] => {
+    const limits = findLimits(coordinates);
+    const grid = [];
+    for (let row = 0; row <= limits[1]; row++) {
+        let rowMap = '';
+        for (let column = 0; column <= limits[0]; column++) {
+            rowMap = rowMap + detectClosestCoordinate(coordinates, column, row);
+        }
+        grid.push(rowMap);
+    }
+    return grid;
+};
+
+export const findInfiniteAreas = (grid: string[]): string[] => {
+    const infinites = [];
+    grid.forEach((row, rowNumber) => {
+        row.split('').forEach((location, locationNumber) => {
+            if (!(rowNumber === 0 || rowNumber === (grid.length - 1) || locationNumber === 0 || locationNumber === (location.length - 1))) {
+                return;
+            }
+            if (location === '+' || location === '.') {
+                return;
+            }
+            if (infinites.indexOf(location) < 0) {
+                infinites.push(location);
+            }
+        });
+    });
+    return infinites.sort();
+};
+
+export const findSizeOfLargestFiniteArea = (coordinates: Coordinates): number => {
+    const grid = mapGrid(coordinates);
+    const infinites = findInfiniteAreas(grid);
+    let largest = {
+        size: 0,
+        id: ''
+    };
+    coordinates.forEach((coordinate) => {
+        if (infinites.indexOf(coordinate.id) >= 0) {
+            return;
+        }
+        let size = 0;
+        grid.forEach(row => {
+            const locations = row.split('');
+            size += locations.filter(location => location === coordinate.id).length;
+        });
+        if (size > largest.size) {
+            largest = {
+                size: size,
+                id: coordinate.id
+            };
+        }
+    });
+    return largest.size;
+};
+
+export const parse = (rawInputs: string[], _log: Logger): Coordinates => {
+    const ids = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    return rawInputs.map((input, index) => {
+        const parts = input.split(', ');
+        return {
+            id: ids[index],
+            column: parseInt(parts[0], 10),
+            row: parseInt(parts[1], 10)
+        }
+    });
+};
+
+export const run1 = (coordinates: Coordinates, _log: Logger): number => {
+    return findSizeOfLargestFiniteArea(coordinates);
+};
---
+++ test/day6/test.ts
@@ -0,0 +1,128 @@
+import { test } from 'ava';
+import {
+    detectClosestCoordinate,
+    findInfiniteAreas,
+    findLimits,
+    findSizeOfLargestFiniteArea,
+    mapGrid, parse
+} from '../../src/day6/main';
+
+test('Calculate limits of grid', t => {
+    const coordinates = [
+        { id: 'A', column: 1, row: 1 },
+        { id: 'B', column: 1, row: 6 },
+        { id: 'C', column: 8, row: 3 },
+        { id: 'D', column: 3, row: 4 },
+        { id: 'E', column: 5, row: 5 },
+        { id: 'F', column: 8, row: 9 }
+    ];
+    t.deepEqual(findLimits(coordinates), [8, 9]);
+});
+
+test('Detect closest coordinate to square', t => {
+    const coordinates = [
+        { id: 'A', column: 1, row: 1 },
+        { id: 'B', column: 1, row: 6 },
+        { id: 'C', column: 8, row: 3 },
+        { id: 'D', column: 3, row: 4 },
+        { id: 'E', column: 5, row: 5 },
+        { id: 'F', column: 8, row: 9 }
+    ];
+    t.is(detectClosestCoordinate(coordinates, 7, 4), 'C');
+});
+
+test('Detect closest coordinate to square if square is a coordinate', t => {
+    const coordinates = [
+        { id: 'A', column: 1, row: 1 },
+        { id: 'B', column: 1, row: 6 },
+        { id: 'C', column: 8, row: 3 },
+        { id: 'D', column: 3, row: 4 },
+        { id: 'E', column: 5, row: 5 },
+        { id: 'F', column: 8, row: 9 }
+    ];
+    t.is(detectClosestCoordinate(coordinates, 3, 4), 'D');
+});
+
+test('Detect multiple closest coordinates to square', t => {
+    const coordinates = [
+        { id: 'A', column: 1, row: 1 },
+        { id: 'B', column: 1, row: 6 },
+        { id: 'C', column: 8, row: 3 },
+        { id: 'D', column: 3, row: 4 },
+        { id: 'E', column: 5, row: 5 },
+        { id: 'F', column: 8, row: 9 }
+    ];
+    t.is(detectClosestCoordinate(coordinates, 0, 4), '.');
+});
+
+test('Map grid', t => {
+    const coordinates = [
+        { id: 'A', column: 1, row: 1 },
+        { id: 'B', column: 1, row: 6 },
+        { id: 'C', column: 8, row: 3 },
+        { id: 'D', column: 3, row: 4 },
+        { id: 'E', column: 5, row: 5 },
+        { id: 'F', column: 8, row: 9 }
+    ];
+    const expected = [
+        'AAAAA.CCC',
+        'AAAAA.CCC',
+        'AAADDECCC',
+        'AADDDECCC',
+        '..DDDEECC',
+        'BB.DEEEEC',
+        'BBB.EEEE.',
+        'BBB.EEEFF',
+        'BBB.EEFFF',
+        'BBB.FFFFF'
+    ];
+    t.deepEqual(mapGrid(coordinates), expected);
+});
+
+test('Find infinite areas', t => {
+    const grid = [
+        'AAAAA.CCC',
+        'AAAAA.CCC',
+        'AAADDECCC',
+        'AADDDECCC',
+        '..DDDEECC',
+        'BB.DEEEEC',
+        'BBB.EEEE.',
+        'BBB.EEEFF',
+        'BBB.EEFFF',
+        'BBB.FFFFF'
+    ];
+    t.deepEqual(findInfiniteAreas(grid), ['A', 'B', 'C', 'F']);
+});
+
+test('Find size of largest finite area', t => {
+    const coordinates = [
+        { id: 'A', column: 1, row: 1 },
+        { id: 'B', column: 1, row: 6 },
+        { id: 'C', column: 8, row: 3 },
+        { id: 'D', column: 3, row: 4 },
+        { id: 'E', column: 5, row: 5 },
+        { id: 'F', column: 8, row: 9 }
+    ];
+    t.is(findSizeOfLargestFiniteArea(coordinates), 17);
+});
+
+test('Parse input', t => {
+    const input = [
+        '1, 1',
+        '1, 6',
+        '8, 3',
+        '3, 4',
+        '5, 5',
+        '8, 9'
+    ];
+    const coordinates = [
+        { id: 'A', column: 1, row: 1 },
+        { id: 'B', column: 1, row: 6 },
+        { id: 'C', column: 8, row: 3 },
+        { id: 'D', column: 3, row: 4 },
+        { id: 'E', column: 5, row: 5 },
+        { id: 'F', column: 8, row: 9 }
+    ];
+    t.deepEqual(parse(input, null), coordinates);
+});
